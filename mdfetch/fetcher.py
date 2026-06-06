5ae8f0772065da2329e69910a7d0b03671e0afb0
AmanSg098 <singhaman123890@gmail.com>
2026-06-06 12:45:26 +0530

    Added cookies, proxies and params support as well as retries on failure

--- mdfetch/fetcher.py
+++ mdfetch/fetcher.py
@@ -1,6 +1,9 @@
 # /fetcher.py
 import requests
 
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
 from .page import Page
 
 
@@ -8,11 +11,34 @@ def fetch(
     url: str,
     timeout: int = 30,
     headers: dict | None = None,
+    cookies: dict | None = None,
+    proxies: dict | None = None,
+    params: dict | None = None,
+    retries: int = 3,
 ):
-    response = requests.get(
+    session = requests.Session()
+
+    retry_strategy = Retry(
+        total=retries,
+        backoff_factor=1,
+        status_forcelist=[429,500,502,503,504],
+    )
+
+    adapter = HTTPAdapter(
+        max_retries=retry_strategy
+    )
+
+
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+
+    response = session.get(
         url,
         timeout=timeout,
         headers=headers,
+        cookies=cookies,
+        proxies=proxies,
+        params=params,
     )
 
     return Page(
